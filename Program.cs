5cdfca4f1ce2101357159e2ea3f69b6e9272ed1f
Anandmohanty <mohantyanand3@gmail.com>
2023-04-18 23:02:25 +0530

    [Anand] - Types of Constructor

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Day_3B_OppsConcept
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Day 3B Practice Problem ");
+
+            //Type of Constructor//
+            TypeOfConstructor typeOfConstructor = new TypeOfConstructor();
+
+            TypeOfConstructor parameterConstructor = new TypeOfConstructor("Mahesh", 30);
+
+            TypeOfConstructor copyConstructor = new TypeOfConstructor(parameterConstructor);
+
+            TypeOfConstructor.SampleMessage();
+        }
+    }
+}
