58e4bdb311a01533f20ec1f62b7ebae904c48e80
Anandmohanty <mohantyanand3@gmail.com>
2023-04-18 23:22:22 +0530

    [Anand] - Access Modifier

--- Program.cs
+++ Program.cs
@@ -21,6 +21,18 @@ namespace Day_3B_OppsConcept
             //Interface//
             BankManager bankManager = new BankManager();
             bankManager.GetBankDetails();
+
+            //Access Modifier//
+            AccessModifier accessModifier = new AccessModifier();
+            Console.WriteLine("We can access bank account number variable due to Public access modifier\n Bank Account number is " + accessModifier.bankAccountNumber);
+            BankAccount bankAccount = new BankAccount();
+            Console.WriteLine("This is Example of Private Access modifier \n like now we can not get balance directly but \n we can use this by public method with same class " + bankAccount.GetBalance());
+
+            Rectangle rectangle = new Rectangle(25, 60);
+
+            int area = rectangle.GetArea();
+
+            Console.WriteLine("Area of the given value is = " + area);
         }
     }
 }
